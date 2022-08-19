fa63d67b368b086b7ea8bef47e520362a31a84d9
Afsan <khanafsaan11@gmail.com>
2022-08-20 01:30:39 +0530

    primitive data type

---
+++ part_2_primitive/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        int roll_no = 64;
+        char letter = 'r';
+        float marks = 100.4f;
+        double large_decimal_numbers = 4567879.90;
+        long large_integers_numbers = 10101010;
+        boolean check = false;
+    }
+}
---
+++ part_2_primitive/Readme.md
@@ -0,0 +1,13 @@
+Data types
+
+1. primitive data type - datatype that cannot be broken down into any other data type
+    - int
+    - long
+    - double
+    - char
+    - bool
+    - float
+
+2. derived data type - data type that can be broken down in any other data type
+    - string
+    - array
