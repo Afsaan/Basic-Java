4bf9e6feb17da3664dd4dbbbe0738a0b0d000714
Afsan <khanafsaan11@gmail.com>
2022-08-25 23:16:59 +0530

    added type casting

---
+++ part_3_type_casting/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args) {
+        int num = (int)(67.678f);
+        float float_num = (float)(67);
+
+        System.out.println(num);
+        System.out.println(float_num);
+
+
+    }
+};
---
+++ part_3_type_casting/Readme.md
@@ -0,0 +1,7 @@
+Type casting
+
+- converting one data type to another is called as type casting
+
+-  When we can apply typecasting
+    1. the two should be compatiable
+    2. destination type should greater than source type
