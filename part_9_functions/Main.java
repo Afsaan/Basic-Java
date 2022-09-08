331ed28f41dee6f3be1965a1be6a777e3b9d2a28
Afsan <khanafsaan11@gmail.com>
2022-09-08 07:58:41 +0530

    welcome to functions

---
+++ part_9_functions/Main.java
@@ -0,0 +1,11 @@
+package part_9_functions;
+
+public class Main {
+    static void greetings(){
+        System.out.println("welcome to function");
+    }
+
+    public static void main(String[] args) {
+        greetings();
+    }
+}
